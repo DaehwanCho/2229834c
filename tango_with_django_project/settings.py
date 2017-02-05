e0369802e09213a3f20ba42a3f3b09f906c420e3
DAEHWAN CHO <DAEHWAN CHO>
2017-02-05 02:44:15 +0000

    chapter 8 05/02

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -90,14 +90,13 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(os.path.dirname(__file__)), 'db.sqlite3'),
-
-    }
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+}
+
 
 
 # Password validation
