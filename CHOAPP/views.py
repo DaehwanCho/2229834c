2bc43a232ee6c70b99232e9ce3ad04a33236d77a
DAEHWAN CHO <DAEHWAN CHO>
2017-02-09 18:23:22 +0000

    removechoapp

--- CHOAPP/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("CHOAPP says hey there partner!")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'CHOAPP',
-
+   
 
 
 
